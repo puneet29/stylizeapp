d2b6fffaca737cc14b08d36fa0c65488915688a6
Puneet Saini <puneet29saini@gmail.com>
2019-06-16 21:31:50 +0530

    Fixed blending

--- utils.py
+++ utils.py
@@ -24,11 +24,15 @@ def load_image(filename):
 
 
 def match_size(image, imageToMatch):
-    img = image.squeeze(0)
+    img = image.clone().clamp(0, 255).numpy()
+    img = img.squeeze(0)
+    img = img.transpose(1, 2, 0).astype('uint8')
+    img = Image.fromarray(img)
+    img = img.resize(
+        (imageToMatch.shape[3], imageToMatch.shape[2]), Image.ANTIALIAS)
     transform = transforms.Compose([
-        transforms.ToPILImage(),
-        transforms.Resize((imageToMatch.shape[2], imageToMatch.shape[3])),
-        transforms.ToTensor()
+        transforms.ToTensor(),
+        transforms.Lambda(lambda x: x.mul(255))
     ])
     img = transform(img)
     return(img)
